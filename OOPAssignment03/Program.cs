481cd248bb1c8982b2e471d62d00ff9f62fe74b6
hamedmahmouddev7-debug <hamedmahmoud.dev7@gmail.com>
2026-08-11 07:32:13 +0300

    Question01

--- OOPAssignment03/Program.cs
+++ OOPAssignment03/Program.cs
@@ -1,9 +1,17 @@
-﻿namespace OOPAssignment03
+﻿using System.Text;
+
+namespace OOPAssignment03
 {    internal class Program
     {
         static void Main(string[] args)
         {
-           
+            #region Question01
+              // (A) Overloading: Same method name, but different parameters in the same class.
+             //      Overriding: A child class changes the method of the parent class.
+
+            // (B) Static Binding: The method to call is decided at compile time.
+           //      Dynamic Binding: The method to call is decided at runtime.
+            #endregion
         }
     }
 }   
