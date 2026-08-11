a8bc91aefb4580f707972d654c0fc24150065ad8
hamedmahmouddev7-debug <hamedmahmoud.dev7@gmail.com>
2026-08-11 07:53:16 +0300

    Question02

--- OOPAssignment03/Program.cs
+++ OOPAssignment03/Program.cs
@@ -6,11 +6,21 @@ namespace OOPAssignment03
         static void Main(string[] args)
         {
             #region Question01
-              // (A) Overloading: Same method name, but different parameters in the same class.
-             //      Overriding: A child class changes the method of the parent class.
+            // (A) Overloading: Same method name, but different parameters in the same class.
+            //      Overriding: A child class changes the method of the parent class.
 
             // (B) Static Binding: The method to call is decided at compile time.
-           //      Dynamic Binding: The method to call is decided at runtime.
+            //      Dynamic Binding: The method to call is decided at runtime.
+            #endregion
+
+            #region Question02
+            // (A) sealed on a class prevents any other class from inheriting from it.
+
+
+            // (B) A sealed class blocks inheritance from the entire class. A sealed method only blocks that specific overridden method from being overridden again further down the hierarchy — the class itself can still be inherited
+
+
+            //  (C)  No, it cannot be overridden again. Once a method is marked sealed override, it stops the override chain — no further derived class can override it.
             #endregion
         }
     }
